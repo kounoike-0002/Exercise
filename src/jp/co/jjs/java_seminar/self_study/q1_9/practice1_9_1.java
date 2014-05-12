4bfe48aa8b4bcbeb3a2d2baa89ac3c22503a6f8a
kounoike <jjs-0002@outlook.jp>
2014-05-12 17:08:46 +0900

    test commit

--- src/jp/co/jjs/java_seminar/self_study/q1_9/practice1_9_1.java
+++ src/jp/co/jjs/java_seminar/self_study/q1_9/practice1_9_1.java
@@ -1,6 +1,6 @@
 package jp.co.jjs.java_seminar.self_study.q1_9;
 
-public class practice1_9_1 {
+public class Practice1_9_1 {
     public static void main(String args[]){
         int num = 1000;
         System.out.println(num+" is the value of num");
