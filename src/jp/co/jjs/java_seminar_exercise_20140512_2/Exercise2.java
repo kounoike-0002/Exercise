3da66fd2722c716b1df7a3e7ba08a0b7f3666f0a
kounoike <jjs-0002@outlook.jp>
2014-05-12 16:13:07 +0900

    exercise2

---
+++ src/jp/co/jjs/java_seminar_exercise_20140512_2/Exercise2.java
@@ -0,0 +1,13 @@
+package jp.co.jjs.java_seminar_exercise_20140512_2;
+
+public class Exercise2 {
+    public static void main(String args[]){
+        String tenki = "曇り";
+        double kion = 24.0;
+        
+        System.out.println("今日の天気は"+tenki+"です");
+        System.out.println("今日の気温は"+kion+"℃です");
+        
+    }
+
+}
