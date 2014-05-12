a58e5320cbd1cda11f492ef33068713d4359f4ab
kounoike <jjs-0002@outlook.jp>
2014-05-12 17:27:13 +0900

    rest repository

---
+++ src/jp/co/jjs/java_seminar/exercise_20140512_1/Exercise1.java
@@ -0,0 +1,23 @@
+package jp.co.jjs.java_seminar.exercise_20140512_1;
+
+public class Exercise1 {
+    public static void main (String args[]){
+        int a = 1;
+        short b = 2;
+        long c = 3;
+        byte d = 4;
+        double e = 5.0;
+        float f = 6.0f;
+        boolean g = true;
+        char h = '7';
+
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+        System.out.println("c="+c);
+        System.out.println("d="+d);
+        System.out.println("e="+e);
+        System.out.println("f="+f);
+        System.out.println("g="+g);
+        System.out.println("h="+h);
+    }
+}
