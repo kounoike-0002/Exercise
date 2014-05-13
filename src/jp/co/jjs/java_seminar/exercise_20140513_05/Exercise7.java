01ec9b92c2c2cbb7b88c57ee2788d00b704e822c
kounoike <jjs-0002@outlook.jp>
2014-05-13 13:22:10 +0900

    exeecise

---
+++ src/jp/co/jjs/java_seminar/exercise_20140513_05/Exercise7.java
@@ -0,0 +1,17 @@
+package jp.co.jjs.java_seminar.exercise_20140513_05;
+
+public class Exercise7 {
+    public static void main(String[] args) {
+        int n = 20;
+         
+        if(((n % 4) == 0) && ((n % 5) == 0)){
+            System.out.println("20の倍数です");
+        }else if((n % 4) == 0){
+                System.out.println("4の倍数です");
+        }else if((n % 5) == 0){
+                System.out.println("5の倍数です");
+        }    
+    }
+ }
+
+
