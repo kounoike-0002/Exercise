f0d1eefa40a697c965868288bb094d3d1617d3b4
kounoike <jjs-0002@outlook.jp>
2014-05-14 11:23:13 +0900

    secound commit

--- src/jp/co/jjs/java_seminar/exercise_20140514_01/Book.java
+++ src/jp/co/jjs/java_seminar/exercise_20140514_01/Book.java
@@ -1,5 +1,11 @@
 package jp.co.jjs.java_seminar.exercise_20140514_01;
 
 public class Book {
-
+    String bookName;        //本のタイトル
+    int bookNedan;          //本の値段
+    String bookWriter;      //本著者
+    String isbn;            //ISBN番号
+    String publisher;       //本の出版社
+    boolean bookRent;       //本の貸し出し情報
+    String firstEditionDate;//本の初版日
 }
